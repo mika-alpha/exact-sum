a393423620755237e7f0b1bdfa74b54a0b4e65b1
fuuka-anemone <78612129+fuuka-anemone@users.noreply.github.com>
2021-03-13 13:54:23 -0500

    polished and accepted.
    fixed some errors and now it has been accepted in online judge (uva).

--- src/Main.java
+++ src/Main.java
@@ -9,41 +9,36 @@ public class Main {
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
-        int size = Integer.parseInt(br.readLine());
         String line = br.readLine();
-        String[] data = line.split(" ");
-        array = new int[size];
-        for (int i = 0; i < data.length; i++) {
-            array[i] = Integer.parseInt(data[i]);
+        while(line!=null) {
+            int size = Integer.parseInt(line);
+            String[] data = br.readLine().split(" ");
+            array = new int[size];
+            for (int i = 0; i < data.length; i++) {
+                array[i] = Integer.parseInt(data[i]);
+            }
+            int m = Integer.parseInt(br.readLine());
+            Arrays.sort(array);
+            findSolutions(m);
+            bw.write("Peter should buy books whose prices are " + array[pSolutions[0]] + " and " + array[pSolutions[1]] + ".\n\n");
+            br.readLine();
+            line = br.readLine();
         }
-        int m = Integer.parseInt(br.readLine());
-        Arrays.sort(array);
-        findSolutions(m);
-        bw.write("Peter should buy books whose prices are "+ array[pSolutions[0]] + " and "+ array[pSolutions[1]]+".\n");
         br.close();
         bw.close();
     }
 
-    public static int binarySearch(int x, int takenPos) {
+    public static int binarySearch(int x) {
         int pos = -1;
         int s = 0;
         int e = array.length - 1;
         while (s <= e && pos < 0) {
             int mp = (s + e) / 2;
-            if (array[mp] == x && mp != takenPos) {
+            if (array[mp] == x) {
                 pos = mp;
             } else if (array[mp] < x) {
                 s = mp + 1;
-            } else if (array[mp] == x && takenPos == mp) {
-                try {
-                    if (array[mp + 1] == x) {
-                        pos = mp + 1;
-                    } else if (array[mp - 1] == x) {
-                        pos = mp - 1;
-                    }
-                } catch (Exception ignore) {
-                }
-            } else {
+            } else if (array[mp] > x){
                 e = mp - 1;
             }
         }
@@ -51,18 +46,31 @@ public class Main {
     }
 
     public static void findSolutions(int m) {
-        int i = m / 2;
-        int j = m / 2;
-        if (m % 2 != 0) {
-            i = (m / 2) - 1;
-        }
         pSolutions[0] = -1;
         pSolutions[1] = -1;
-        while (pSolutions[0] == -1 && pSolutions[1] == -1) {
-            pSolutions[0] = binarySearch(i, pSolutions[1]);
-            pSolutions[1] = binarySearch(j, pSolutions[0]);
-            j++;
+        int i = m / 2;
+        int j = (m+1) / 2;
+        if (m % 2 == 0) {
+            pSolutions[0] = binarySearch(i);
+            if (pSolutions[0] != -1){
+                if ((pSolutions[0]+1) < array.length){
+                    if (array[pSolutions[0]+1] == i){
+                        pSolutions[1] = pSolutions[0]+1;
+                    }
+                } else if ((pSolutions[0]-1) >= 0){
+                    if (array[pSolutions[0]-1] == i){
+                        pSolutions[1] = pSolutions[0]-1;
+                    }
+                }
+            }
             i--;
+            j++;
+        }
+        while (pSolutions[0] < 0 || pSolutions[1] < 0) {
+            pSolutions[0] = binarySearch(i);
+            pSolutions[1] = binarySearch(j);
+            i--;
+            j++;
         }
     }
 }
